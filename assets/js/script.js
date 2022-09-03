19422de35359fffb288964d5ec923931abc16f21
Amanj-18 <89749348+Amanj-18@users.noreply.github.com>
2022-09-03 18:49:07 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,43 @@ const calculateTemp = () => {
         let fahrenheit = Math.round((cel* 9/5) + 32);
         return fahrenheit;
     }
-    const fahToCel = (Fah) =>{
-        let celsius = Math.round((Fah -32 )* 5/9);
+    const celToKel = (cel) =>{
+        let kelvin = Math.round((cel + 273.15));
+        return kelvin;
+    }
+    const fahToCel = (fah) =>{
+        let celsius = Math.round((fah -32 )* 5/9);
         return celsius ;
     }
+    const fahToKel = (fah) =>{
+        let kelvin = Math.round(((fah-32)*(5/9)) + 273.15);
+        return kelvin ;
+    }
+    const kelToCel = (kel) =>{
+        let celsius = Math.round(kel - 273.15);
+        return celsius ;
+    }
+    const kelToFah = (kel) =>{
+        let fahrenheit = Math.round(1.8*(kel-273.15) + 32);
+        return fahrenheit ;
+    }
 
-    let result;
+    let result1;
+    let result2;
 
     if (valueTemp == 'cel') {
-        result = celToFah(numberTemp)
-        document.getElementById('resultContainer').innerHTML= ` =  ${result}  ° Fahrenheit `;
+        result1 = celToFah(numberTemp)
+        result2 = celToKel(numberTemp)
+        document.getElementById('resultContainer').innerHTML= ` =  ${result1}  ° Fahrenheit and ${result2} Kelvin `;
+    }
+    else if(valueTemp == 'fah') {
+        result1 = fahToCel(numberTemp)
+        result2 = fahToKel(numberTemp)
+        document.getElementById('resultContainer').innerHTML= ` =  ${result1}  ° Celcius and ${result2} Kelvin `;
     }
     else{
-        result = fahToCel(numberTemp)
-        document.getElementById('resultContainer').innerHTML= ` =  ${result}  ° Celcius `;
+        result1 = kelToCel(numberTemp)
+        result2 = kelToFah(numberTemp)
+        document.getElementById('resultContainer').innerHTML= ` =  ${result1}  ° Celcius and ${result2} ° Fahrenheit `;
     }
-
 }
